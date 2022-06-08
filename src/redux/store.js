4a14f93a4d89f827dd47bc3a242ac42e48b547ba
Byenguk-An <quddnr005@naver.com>
2022-06-09 06:33:56 +0900

    redux initial setting

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,11 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux";
+import rootReducer from "./reducers/index";
 
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
